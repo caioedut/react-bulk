0357d01b78c1249488034546be7d308b29a5b57a
Caio Teixeira <caioedut@gmail.com>
2022-06-14 01:32:35 -0300

    Themes

--- packages/web/dist/src/Box/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const react_1 = require("react");
-const core_1 = require("@react-bulk/core");
-const Box = (0, react_1.forwardRef)((props, ref) => {
-    return (0, core_1.createBox)(props, ref, 'div');
-});
-exports.default = Box;
