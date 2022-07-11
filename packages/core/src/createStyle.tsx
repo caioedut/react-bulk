24301329308194398a76c7e1d40faed156fc67e8
Caio Teixeira <caioedut@gmail.com>
2022-07-11 13:26:40 -0300

    Fix Theme

--- packages/core/src/createStyle.tsx
+++ packages/core/src/createStyle.tsx
@@ -1,7 +1,8 @@
 import { useEffect, useMemo, useRef } from 'react';
 
+import { useTheme } from '@react-bulk/core';
+
 import Platform from './Platform';
-import { useTheme } from './ReactBulk';
 import css from './styles/css';
 import jss from './styles/jss';
 import crypt from './utils/crypt';
