d6437c52cdc42bad10526e5e2bd272ce49058473
Caio Teixeira <caioedut@gmail.com>
2022-07-11 11:15:38 -0300

    Dropdown and Select

--- packages/native/src/index.ts
+++ packages/native/src/index.ts
@@ -3,6 +3,7 @@ export { default as Button } from './Button';
 export { default as Card } from './Card';
 export { default as Collapse } from './Collapse';
 export { default as Divider } from './Divider';
+export { default as Dropdown } from './Dropdown';
 export { default as Input } from './Input';
 export { default as Image } from './Image';
 export { default as Modal } from './Modal';
