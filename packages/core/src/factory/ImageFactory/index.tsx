173a28b4fd88cf999f5d189517113bcf26ad7cb5
Caio Teixeira <caioedut@gmail.com>
2022-08-01 20:00:37 -0300

    Image improvements

--- packages/core/src/factory/ImageFactory/index.tsx
+++ packages/core/src/factory/ImageFactory/index.tsx
@@ -1,20 +1,33 @@
 import React, { useRef } from 'react';
 
+import { useTheme } from '@react-bulk/core';
+
 import Platform from '../../Platform';
 import { ImageProps } from '../../types';
 import BoxFactory from '../BoxFactory';
 
-function ImageFactory({ width, height, mode = 'cover', style, map, ...rest }: ImageProps | any, ref: any) {
+function ImageFactory({ width, height, mode, corners, rounded, style, map, ...rest }: ImageProps | any, ref: any) {
+  const theme = useTheme();
   const { web, native } = Platform;
   const { Image } = map;
 
   const defaultRef: any = useRef(null);
   const imageRef = ref || defaultRef;
 
+  mode = mode ?? 'cover';
+
   style = [
     web && { display: 'block', objectFit: mode },
 
     native && { resizeMode: mode === 'fill' ? 'stretch' : mode },
+
+    corners && {
+      borderRadius: corners * theme.shape.borderRadius,
+    },
+
+    rounded && {
+      borderRadius: Math.min(height, width) / 2,
+    },
 
     style,
 
