b8bd4bdc5b1bb6e50ea76928106dd9ef2126af7b
Caio Teixeira <caioedut@gmail.com>
2022-08-08 15:25:36 -0300

    Fix Collapse (Web)

--- packages/web/src/Collapse/index.tsx
+++ packages/web/src/Collapse/index.tsx
@@ -1,4 +1,4 @@
-import { ComponentPropsWithRef, forwardRef, useEffect, useRef, useState } from 'react';
+import { ComponentPropsWithRef, forwardRef, useEffect, useRef } from 'react';
 
 import { BoxFactory, CollapseFactory, CollapseProps } from '@react-bulk/core';
 
@@ -9,69 +9,90 @@ export type CollapsePropsWeb = ComponentPropsWithRef<'div'> & CollapseProps;
 function Collapse({ in: shown, ...props }: CollapsePropsWeb, ref) {
   const map = useMap();
 
-  const readyRef = useRef(false);
+  const { current: startHidden } = useRef(!shown);
   const containerRef = useRef<HTMLElement>(null);
+  const isShown = containerRef?.current?.style?.height?.replace?.(/\D/g, '') !== '0';
 
-  const [height, setHeight] = useState<any>(null);
+  console.log('startHidden', startHidden);
+
+  // const transition = 'height 0.25s ease';
+
+  // useEffect(() => {
+  //   const $el = containerRef.current;
+  //
+  //   if ($el) {
+  //     $el.style.height = '';
+  //   }
+  // }, [height]);
 
   useEffect(() => {
-    const $el = containerRef.current;
-
-    if ($el) {
-      $el.style.height = '';
-    }
-  }, [height]);
-
-  useEffect(() => {
-    const $el = containerRef.current;
-    if (!$el) return;
-
-    const clear = () => window.getComputedStyle($el).height.replace(/\D/g, '') !== '0' && setHeight(null);
-
-    $el.addEventListener('transitionend', clear);
-
-    return () => $el.removeEventListener('transitionend', clear);
+    // const $el = containerRef.current;
+    // if (!$el) return;
+    //
+    // const clear = () => {
+    //   // $el.style.transition = '';
+    //   // if (window.getComputedStyle($el).height.replace(/\D/g, '') !== '0') {
+    //   // $el.style.height = '';
+    //   // }
+    // };
+    //
+    // $el.addEventListener('transitionend', clear);
+    //
+    // return () => $el.removeEventListener('transitionend', clear);
   }, []);
 
   useEffect(() => {
-    const $el = containerRef.current;
-    if (!$el) return;
-
-    if (!readyRef.current) {
-      readyRef.current = true;
-      return;
-    }
-
-    let curHeight = window.getComputedStyle($el).height;
-    let newHeight = '0px';
-
-    $el.style.transition = 'none';
-    $el.style.height = 'auto';
-
-    if (shown) {
-      curHeight = '0px';
-      newHeight = window.getComputedStyle($el).height;
-    }
-
-    $el.style.height = curHeight;
-    $el.style.transition = '';
-
-    setHeight(newHeight);
+    shown ? show() : hide();
   }, [shown]);
 
-  const constainerStyle = [
-    {
-      overflow: 'hidden',
-      transition: 'height 0.2s ease',
-    },
+  const show = () => {
+    const $el = containerRef.current;
+    if (isShown || !$el) return;
 
-    !readyRef.current && !shown && { height: '0px' },
+    $el.style.transition = 'none';
+    $el.style.height = '';
 
-    height !== null && { height },
-  ];
+    const height = `${$el.getBoundingClientRect().height}px`;
+
+    $el.style.height = '0px';
+    $el.style.transition = '';
+
+    const complete = () => {
+      $el.style.height = '';
+      $el.removeEventListener('transitionend', complete);
+    };
+
+    $el.addEventListener('transitionend', complete);
+
+    setTimeout(() => ($el.style.height = height), 0);
+  };
+
+  const hide = () => {
+    const $el = containerRef.current;
+    if (!isShown || !$el) return;
+
+    $el.style.transition = 'none';
+    $el.style.height = '';
+
+    const height = `${$el.getBoundingClientRect().height}px`;
+
+    $el.style.height = height;
+    $el.style.transition = '';
+    $el.style.display = '';
+
+    setTimeout(() => ($el.style.height = '0px'), 0);
+  };
 
   return (
-    <BoxFactory ref={containerRef} style={constainerStyle} map={map}>
+    <BoxFactory
+      map={map}
+      ref={containerRef}
+      style={{
+        overflow: 'hidden',
+        transition: 'height 0.25s ease',
+      }}
+      rawStyle={startHidden && { display: 'none' }}
+    >
       <CollapseFactory ref={ref} {...props} map={map} />
     </BoxFactory>
   );
