b5a0ba54815a8452315b5e1e08ac9abebc0a8cf1
Caio Teixeira <caioedut@gmail.com>
2022-08-24 21:10:57 -0300

    Native: use "TouchableHighlight" if "Box" has onPress-like events

--- packages/native/src/Box/index.tsx
+++ packages/native/src/Box/index.tsx
@@ -1,10 +1,15 @@
 import { forwardRef } from 'react';
+import { TouchableHighlight } from 'react-native';
 
 import { BoxFactory, BoxProps } from '@react-bulk/core';
 
 import useMap from '../useMap';
 
 function Box({ ...props }: BoxProps, ref) {
+  if ((props?.onPress || props?.onPressIn || props?.onPressOut || props?.onClick) && !props?.component) {
+    props.component = TouchableHighlight;
+  }
+
   return <BoxFactory ref={ref} {...props} map={useMap()} />;
 }
 
