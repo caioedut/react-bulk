dce4ed47f8157e670fecee1d356dc62b96d54639
Caio Teixeira <caioedut@gmail.com>
2022-08-24 21:17:40 -0300

    Native: use "TouchableOpacity" if "Box" has onPress-like events

--- packages/native/src/Box/index.tsx
+++ packages/native/src/Box/index.tsx
@@ -1,13 +1,15 @@
 import { forwardRef } from 'react';
-import { TouchableHighlight } from 'react-native';
+import { TouchableOpacity } from 'react-native';
 
 import { BoxFactory, BoxProps } from '@react-bulk/core';
 
 import useMap from '../useMap';
 
 function Box({ ...props }: BoxProps, ref) {
-  if ((props?.onPress || props?.onPressIn || props?.onPressOut || props?.onClick) && !props?.component) {
-    props.component = TouchableHighlight;
+  if ((props.onPress || props.onPressIn || props.onPressOut || props.onClick) && !props.component) {
+    // @ts-ignore
+    props.activeOpacity = props.activeOpacity ?? 1;
+    props.component = TouchableOpacity;
   }
 
   return <BoxFactory ref={ref} {...props} map={useMap()} />;
