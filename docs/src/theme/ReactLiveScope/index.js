ce2a8f975f76137bbd2807488855aec908ed5c4b
Caio Teixeira <caioedut@gmail.com>
2023-01-09 20:06:52 -0300

    docs: fix React Live Scope bug

--- docs/src/theme/ReactLiveScope/index.js
+++ docs/src/theme/ReactLiveScope/index.js
@@ -1,14 +1,18 @@
 import React from 'react';
 
-import ReactBulk from '@react-bulk/core';
 import * as Rbk from '@react-bulk/web';
+
+const ReactBulk = Rbk.default;
 
 // Add react-live imports you need here
 const ReactLiveScope = {
-  React,
-  ...React,
   ReactBulk,
   ...Rbk,
+  React,
+  ...React,
 };
+
+// Fix ReactLiveScope bug
+delete ReactLiveScope.default;
 
 export default ReactLiveScope;
