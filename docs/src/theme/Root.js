10dad2b62db1c8d03e1ab152ef4e03efbf0167a1
Caio Teixeira <caioedut@gmail.com>
2023-02-26 19:20:20 -0300

    docs: check localStorage

--- docs/src/theme/Root.js
+++ docs/src/theme/Root.js
@@ -3,7 +3,11 @@ import React from 'react';
 import ReactBulk from '@react-bulk/web';
 
 export default function Root({ children }) {
-  const theme = localStorage.getItem('theme') || 'light';
+  let theme = 'light';
+
+  if (typeof localStorage !== 'undefined') {
+    theme = localStorage.getItem('theme') || 'light';
+  }
 
   return <ReactBulk theme={theme}>{children}</ReactBulk>;
 }
