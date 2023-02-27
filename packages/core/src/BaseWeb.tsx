093c8dccffdc0c712497ead873b3ed51d1316c8c
Caio Teixeira <caioedut@gmail.com>
2023-02-26 23:28:00 -0300

    fix BaseWeb

--- packages/core/src/BaseWeb.tsx
+++ packages/core/src/BaseWeb.tsx
@@ -1,55 +1,52 @@
-import { useMemo } from 'react';
+import { memo } from 'react';
 
 import createMeta from './createMeta';
 import createStyle from './createStyle';
 import css from './styles/css';
 import jss from './styles/jss';
 
-export default function BaseWeb({ theme, children }) {
-  // Run once before render
-  useMemo(() => {
-    createMeta('[charset]', { charset: 'UTF-8' }, false);
-    createMeta('[name="viewport"]', { name: 'viewport', content: 'width=device-width, initial-scale=1.0' }, false);
-  }, []);
+function BaseWeb({ theme, children }) {
+  const scrollBarStyle = css(jss({ theme }, theme.mixins.scroll), 'body');
 
-  // Run on theme change before render
-  useMemo(() => {
-    const scrollBarStyle = css(jss({ theme }, theme.mixins.scroll), 'body');
+  createMeta('[charset]', { charset: 'UTF-8' }, false);
 
-    createStyle({
-      theme,
-      name: 'rbk-global',
-      global: true,
-      prepend: true,
-      style: `
-        *,
-        *:before,
-        *:after {
-          box-sizing: border-box;
-          font-family: inherit;
-        }
+  createMeta('[name="viewport"]', { name: 'viewport', content: 'width=device-width, initial-scale=1.0' }, false);
 
-        ${scrollBarStyle}
+  createStyle({
+    theme,
+    name: 'rbk-global',
+    global: true,
+    prepend: true,
+    style: `
+      *,
+      *:before,
+      *:after {
+        box-sizing: border-box;
+        font-family: inherit;
+      }
 
-        html {
-          font-family: -apple-system, BlinkMacSystemFont, "Segoe UI Variable", "Segoe UI", Roboto, Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol";
-          font-size: ${theme.typography.fontSize}px;
-          line-height: ${theme.typography.lineHeight};
-          height: 100%;
-        }
+      ${scrollBarStyle}
 
-        body {
-          background-color: ${theme.colors.background.secondary};
-          color: ${theme.colors.text.primary};
-          margin: 0;
-          padding: 0;
+      html {
+        font-family: -apple-system, BlinkMacSystemFont, "Segoe UI Variable", "Segoe UI", Roboto, Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol";
+        font-size: ${theme.typography.fontSize}px;
+        line-height: ${theme.typography.lineHeight};
+        height: 100%;
+      }
 
-          height: 100%;
-          width: 100%;
-        }
-      `,
-    });
-  }, [theme]);
+      body {
+        background-color: ${theme.colors.background.secondary};
+        color: ${theme.colors.text.primary};
+        margin: 0;
+        padding: 0;
+
+        height: 100%;
+        width: 100%;
+      }
+    `,
+  });
 
   return children;
 }
+
+export default memo(BaseWeb);
