44f1d526fdbc43075d9e6d701c5198f692bfec7e
Caio Teixeira <caio.teixeira@unyleya.com.br>
2023-07-25 21:15:52 -0300

    refactor(Snackbar): moved to Toaster

---
+++ packages/core/src/hooks/useToaster.ts
@@ -0,0 +1,18 @@
+import { useContext } from 'react';
+
+import { Context } from '../ReactBulk';
+import { ToasterProps } from '../Toaster';
+
+export default function useToaster() {
+  const { toasterRef } = useContext(Context);
+
+  function open(options: string | ToasterProps) {
+    toasterRef?.current?.setProps(typeof options === 'string' ? { content: options } : options);
+  }
+
+  function close() {
+    toasterRef?.current?.setProps(undefined);
+  }
+
+  return { open, close };
+}
