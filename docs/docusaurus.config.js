3615896e68726b836bd3c26bf2657a6c5aace0ce
Caio Teixeira <caio.teixeira@unyleya.com.br>
2023-11-22 14:45:37 -0300

    doc: search

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -94,7 +94,7 @@ const config = {
 
         insights: true,
 
-        contextualSearch: true,
+        contextualSearch: false,
 
         // // Optional: Specify domains where the navigation should occur through window.location instead on history.push. Useful when our Algolia config crawls multiple documentation sites and we want to navigate with window.location.href to them.
         // externalUrlRegex: 'external\\.com|domain\\.com',
