b45a14fe5e51d7933b11444da0dd2d378da6742e
Caio Teixeira <caioedut@gmail.com>
2024-01-02 16:11:33 -0300

    build: fix build dirs

--- scripts/build.ts
+++ scripts/build.ts
@@ -1,15 +1,30 @@
-// import { copyFileSync, readdirSync } from 'fs';
+import { cpSync, existsSync, readdirSync, rmSync } from 'fs';
 import pmex from 'pmex';
 
 pmex('clean');
 
 pmex('lerna exec --parallel -- tsc --build');
 
-// readdirSync('packages', { withFileTypes: true })
-//   .filter((dirent) => dirent.isDirectory())
-//   .forEach((dir) => {
-//     const baseDir = `packages/${dir.name}`;
-//
-//     copyFileSync('README.md', `${baseDir}/dist/README.md`);
-//     copyFileSync(`${baseDir}/package.json`, `${baseDir}/dist/package.json`);
-//   });
+const packages = readdirSync('packages', { withFileTypes: true })
+  .filter((dirent) => dirent.isDirectory())
+  .map((dir) => dir.name);
+
+packages.forEach((dir) => {
+  const baseDir = `packages/${dir}`;
+  const distDir = `${baseDir}/dist`;
+  const subDir = `${distDir}/${dir}/src`;
+
+  if (existsSync(subDir)) {
+    console.log('exists', subDir);
+    cpSync(subDir, distDir, { recursive: true, force: true });
+
+    packages.forEach((item) => {
+      if (existsSync(`${distDir}/${item}`)) {
+        rmSync(`${distDir}/${item}`, { recursive: true, force: true });
+      }
+    });
+  }
+
+  //     copyFileSync('README.md', `${baseDir}/dist/README.md`);
+  //     copyFileSync(`${baseDir}/package.json`, `${baseDir}/dist/package.json`);
+});
