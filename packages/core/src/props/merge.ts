f1ce9a18b7af6d9443c45d17d4ce91ec4ce56891
Caio Teixeira <caioedut@gmail.com>
2024-01-10 15:09:25 -0300

    chore: deepmerge

--- packages/core/src/props/merge.ts
+++ packages/core/src/props/merge.ts
@@ -1,5 +1,3 @@
-// @ts-nocheck
-
 export default function merge(...mixin: unknown[]) {
   let result: any = {};
 
--- packages/core/src/utils/deepmerge.ts
+++ packages/core/src/utils/deepmerge.ts
@@ -1,8 +1,21 @@
-import { all } from 'deepmerge';
-
 export default function deepmerge(...mixed: any[]): (typeof mixed)[0] {
-  return all(
-    mixed.map((obj) => obj ?? {}),
-    { arrayMerge: (_, next) => next },
-  );
+  const result = {};
+
+  for (const item of mixed) {
+    if (!item || typeof item !== 'object') {
+      continue;
+    }
+
+    for (const attr in item) {
+      let value = item[attr];
+
+      if (value && !Array.isArray(value) && typeof value === 'object') {
+        value = deepmerge(result[attr], value);
+      }
+
+      result[attr] = value;
+    }
+  }
+
+  return result;
 }
