da98f513e5c9d9a533a8e69943ddcd0070a3bd99
Caio Teixeira <caioedut@gmail.com>
2024-01-11 15:40:45 -0300

    refactor: begin with cometta

--- packages/core/src/BaseWeb.tsx
+++ packages/core/src/BaseWeb.tsx
@@ -1,51 +1,62 @@
-import { memo } from 'react';
+import { memo, useMemo } from 'react';
+
+import cometta from 'cometta';
 
 import createMeta from './createMeta';
-import createStyle from './createStyle';
-import css from './styles/css';
-import jss from './styles/jss';
 import { AnyObject } from './types';
 
 function BaseWeb({ theme, children }: AnyObject) {
-  const scrollBarStyle = css(jss({ theme }, theme.mixins.scroll), 'body');
-
   createMeta('[charset]', { charset: 'UTF-8' }, false);
 
   createMeta('[name="viewport"]', { name: 'viewport', content: 'width=device-width, initial-scale=1.0' }, false);
 
-  createStyle({
-    theme,
-    name: 'rbk-global',
-    global: true,
-    prepend: true,
-    style: `
-      *,
-      *:before,
-      *:after {
-        box-sizing: border-box;
-        font-family: inherit;
-      }
+  // #HACK
+  useMemo(() => {
+    const scrollBarStyle = Object.entries(theme.mixins.scroll).map(([selector, styles]) => {
+      // @ts-expect-error
+      return `${selector.substring(1)} { ${cometta.css(styles)} }`;
+    });
 
-      ${scrollBarStyle}
+    const style = `
+*,
+*:before,
+*:after {
+  box-sizing: border-box;
+  font-family: inherit;
+}
 
-      html {
-        font-family: -apple-system, BlinkMacSystemFont, "Segoe UI Variable", "Segoe UI", Roboto, Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol";
-        font-size: ${theme.typography.fontSize}px;
-        line-height: ${theme.typography.lineHeight};
-        height: 100%;
-      }
+${scrollBarStyle.join(`\n`)}
 
-      body {
-        background-color: ${theme.colors.background.secondary};
-        color: ${theme.colors.text.primary};
-        margin: 0;
-        padding: 0;
+html {
+  font-family: -apple-system, BlinkMacSystemFont, "Segoe UI Variable", "Segoe UI", Roboto, Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol";
+  font-size: ${theme.typography.fontSize}px;
+  line-height: ${theme.typography.lineHeight};
+  height: 100%;
+}
 
-        height: 100%;
-        width: 100%;
-      }
-    `,
-  });
+body {
+  background-color: ${theme.colors.background.secondary};
+  color: ${theme.colors.text.primary};
+  margin: 0;
+  padding: 0;
+
+  height: 100%;
+  width: 100%;
+}
+    `;
+
+    let tag = document.querySelector('[data-rbk="global"]');
+
+    if (!tag) {
+      tag = document.createElement('style');
+      tag.setAttribute('type', 'text/css');
+      tag.setAttribute('data-rbk', 'global');
+    }
+
+    tag.textContent = style;
+
+    document.head.prepend(tag);
+  }, [theme]);
 
   return children;
 }
