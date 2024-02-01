dda7d3e2bd04e00714da1fd7ee25b59c99f23897
Caio Teixeira <caio.teixeira@unyleya.com.br>
2024-02-01 16:14:22 -0300

    fix: styles and cometta

--- packages/core/src/styles/css.ts
+++ packages/core/src/styles/css.ts
@@ -3,5 +3,5 @@ import cometta, { ComettaParam } from 'cometta';
 import { RbkStyle } from '../types';
 
 export default function css(...mixin: RbkStyle[]) {
-  cometta.css(...(mixin as ComettaParam[]));
+  return cometta.css(...(mixin as ComettaParam[]));
 }
--- packages/core/src/styles/jss.ts
+++ packages/core/src/styles/jss.ts
@@ -3,5 +3,5 @@ import cometta, { ComettaParam } from 'cometta';
 import { RbkStyle } from '../types';
 
 export default function jss(...mixin: RbkStyle[]) {
-  cometta.jss(...(mixin as ComettaParam[]));
+  return cometta.jss(...(mixin as ComettaParam[]));
 }
