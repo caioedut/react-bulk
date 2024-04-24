14bf9330a78e1f22b68752db47ad10dcb4c73830
Caio Teixeira <caio.teixeira@unyleya.com.br>
2024-04-24 18:56:30 -0300

    feat: add new colors

--- examples/native/index.tsx
+++ examples/native/index.tsx
@@ -1,17 +1,18 @@
+import { ThemeEditProps } from '@react-bulk/core';
 import ReactBulk from '@react-bulk/native';
 import { registerRootComponent } from 'expo';
 
 import Main from './src/main';
 
+const theme: ThemeEditProps = {
+  typography: {
+    fontSize: 16,
+  },
+};
+
 function App() {
   return (
-    <ReactBulk
-      theme={{
-        typography: {
-          fontSize: 16,
-        },
-      }}
-    >
+    <ReactBulk theme={theme}>
       <Main />
     </ReactBulk>
   );
