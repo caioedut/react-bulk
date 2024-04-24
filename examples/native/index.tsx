d93296b6e8ae003b8d650d373bff604db9b39378
Caio Teixeira <caio.teixeira@unyleya.com.br>
2024-04-24 18:00:56 -0300

    feat: upgrade examples

--- examples/native/index.tsx
+++ examples/native/index.tsx
@@ -1,21 +1,20 @@
-import { ThemeEditProps } from '@react-bulk/core';
 import ReactBulk from '@react-bulk/native';
 import { registerRootComponent } from 'expo';
 
 import Main from './src/main';
 
-const theme: ThemeEditProps = {
-  typography: {
-    fontSize: 16,
-  },
-};
-
-export default function Index() {
+function App() {
   return (
-    <ReactBulk theme={theme}>
+    <ReactBulk
+      theme={{
+        typography: {
+          fontSize: 16,
+        },
+      }}
+    >
       <Main />
     </ReactBulk>
   );
 }
 
-registerRootComponent(Index);
+registerRootComponent(App);
